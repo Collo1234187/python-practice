fc20a4f380a46b9b154c442883fd8ca96404a45d
Collinstone Odera <collinstoneochieng@gmail.com>
2025-12-07 16:34:22 +0300

    Update fileHandling.py

--- fileHandling.py
+++ fileHandling.py
@@ -1,8 +1,28 @@
-#read - "r"
-#append - "a"
-#write - "w"
+# read  - "r"
+# append - "a"
+# write - "w"
+#readbinary - "rb"
+#writebinary - "wb"
 
-file = open("example.txt, "w")
-file.write("how are you collinstone")
-file.close()
+"""
+file = open("example.txt", "w")   
+file.write("how are you collinstonesssssss")
+file.close()   
+"""
+
+'''
+file = open("example.txt", "r")
+print(file.read())
+'''
+
+'''
+file = open("example.txt", "a")   
+file.write("\n" "you look nice")
+file.close() 
+'''
+
+
+#automatically closes the file — even if errors happen.
+with open("example.txt", "w") as file:
+    file.write("Hello odera")
              
